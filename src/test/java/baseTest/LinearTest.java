e99f3f753aa8f5f804f96005ba5373631a52d01e
Yehia.Metwally <jmetwallym@gmail.com>
2024-10-11 15:48:23 +0300

    test

---
+++ src/test/java/baseTest/LinearTest.java
@@ -0,0 +1,12 @@
+package baseTest;
+
+import org.testng.annotations.Test;
+
+public class LinearTest {
+
+    @Test
+    public void test1()
+    {
+        System.out.println("Yehiaaaaaaaaaaa");
+    }
+}
